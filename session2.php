270ba98980f2c369daf432b3047d41a019b85573
20harshal <42774059+20harshal@users.noreply.github.com>
2019-08-29 17:32:09 +0530

    update session.php

--- session2.php
+++ session2.php
@@ -1,9 +1,4 @@
 ￼<?php
 // Start the session
 session_start();
-$_SESSION["favcolor"] = "green";
-$_SESSION["favanimal"] = "cat";
-echo "Session variables are set.";
-echo "not added";
-echo "how are  you"
 ?>
